ca42be8b2e655f765560120ecd3553ca9de39ed8
jmacnut <jmacnut@gmail.com>
2021-02-23 14:50:28 -1000

    initial commit - completes ASL and Tip Calculator

---
+++ Assets/1 - ASL/ASL.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.ASL
+{
+    public class ASL : MonoBehaviour
+    {
+       [SerializeField] private string _age;
+       [SerializeField] private string _gender;
+       [SerializeField] private string _location;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Age: " + _age + ", Gender: " + _gender + ", Location: " + _location);
+            }
+        }
+    }
+}
---
+++ Assets/2 - Tip Calculator/TipCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// - No redundancy
+/// - Results (total) two decimal places
+/// </summary>
+namespace Assets.TipCalculator
+{
+    public class TipCalculator : MonoBehaviour
+    {
+        [SerializeField] private float _bill = 20f;
+        [SerializeField] private float[] _tips = { 15f, 20f, 25f };
+        [SerializeField] private float _tipCustom = 0f;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Your bill is $" + _bill);
+                foreach (var tip in _tips)
+                {
+                    CalculateTip(tip, false);
+                }
+                CalculateTip(_tipCustom, true);
+            }
+        }
+
+        void CalculateTip(float tip, bool isCustom)
+        {
+            float tipAmount = _bill * tip/100;
+            float total = _bill + tipAmount;
+
+            if (!isCustom)
+            {
+                Debug.Log(tip.ToString() + "% tip = $" + tipAmount + " with a final total of: $" + total.ToString("F2"));
+            }
+            else // custom tip
+            {
+                Debug.Log("Your custom tip of " + tip.ToString() + "% = $" + tipAmount + " with a final total of: $" + total.ToString("F2"));
+            }
+
+        }
+    } 
+}
